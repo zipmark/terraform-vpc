483d374db5de31528fb908d8c091ac7441a21211
Jake Howerton <jake.howerton@gmail.com>
2018-05-23 21:10:02 -0500

    remove provider config

--- variables.tf
+++ variables.tf
@@ -12,23 +12,18 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
 variable "aws_vpc_name" {
   default = "vpc"
 }
 
 variable "aws_azs" {
   description = "comma separated string of availability zones in order of precedence"
-  default = "us-east-1a, us-east-1d, us-east-1e, us-east-1c"
-
+  default     = "us-east-1a, us-east-1d, us-east-1e, us-east-1c"
 }
 
 variable "az_count" {
   description = "number of active availability zones in VPC"
-  default = "3"
+  default     = "3"
 }
 
 variable "vpc_cidr_base" {
@@ -53,11 +48,12 @@ variable "vpc_enable_classiclink" {
 
 variable "admin_subnet_parent_cidr" {
   description = "parent CIDR for the administrative subnets"
-  default = ".0.0/19"
+  default     = ".0.0/19"
 }
 
 variable "admin_subnet_cidrs" {
   description = "CIDRs for the adminsitrative subnets"
+
   default = {
     zone0 = ".0.0/21"
     zone1 = ".8.0/21"
@@ -68,16 +64,17 @@ variable "admin_subnet_cidrs" {
 
 variable "admin_subnet_tags" {
   description = "Tags to apply to the admin subnet"
-  default = {}
+  default     = {}
 }
 
 variable "public_subnet_parent_cidr" {
   description = "parent CIDR for the public subnets"
-  default = ".32.0/19"
+  default     = ".32.0/19"
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDRs for the public subnets"
+
   default = {
     zone0 = ".32.0/21"
     zone1 = ".40.0/21"
@@ -88,16 +85,17 @@ variable "public_subnet_cidrs" {
 
 variable "public_subnet_tags" {
   description = "Tags to apply to the public subnets"
-  default = {}
+  default     = {}
 }
 
 variable "private_prod_subnet_parent_cidr" {
   description = "parent CIDR for the private production subnets"
-  default = ".64.0/19"
+  default     = ".64.0/19"
 }
 
 variable "private_prod_subnet_cidrs" {
   description = "CIDRs for the private production subnets"
+
   default = {
     zone0 = ".64.0/21"
     zone1 = ".72.0/21"
@@ -108,16 +106,17 @@ variable "private_prod_subnet_cidrs" {
 
 variable "private_prod_subnet_tags" {
   description = "Tags to apply to the private production subnets"
-  default = {}
+  default     = {}
 }
 
 variable "private_working_subnet_parent_cidr" {
   description = "parent CIDR for the private working subnets"
-  default = ".96.0/19"
+  default     = ".96.0/19"
 }
 
 variable "private_working_subnet_cidrs" {
   description = "CIDRs for the private working subnets"
+
   default = {
     zone0 = ".96.0/21"
     zone1 = ".104.0/21"
@@ -128,39 +127,40 @@ variable "private_working_subnet_cidrs" {
 
 variable "private_working_subnet_tags" {
   description = "Tags to apply to the private working subnets"
-  default = {}
+  default     = {}
 }
 
 variable "multi_az_nat_gateway" {
   description = "place a NAT gateway in each AZ"
-  default = 1
+  default     = 1
 }
 
 variable "single_nat_gateway" {
   description = "use a single NAT gateway to serve outbound traffic for all AZs"
-  default = 0
+  default     = 0
 }
 
 variable "internet_gateway_tags" {
   description = "Tags to apply to the internet gateway"
-  default = {}
+  default     = {}
 }
 
 variable "public_route_table_tags" {
   description = "Tags to apply to the public route table"
-  default = {}
+  default     = {}
 }
 
 variable "private_route_table_tags" {
   description = "Tags to apply to the private route table"
-  default = {}
+  default     = {}
 }
 
 variable "global_tags" {
   description = "AWS tags that will be added to all resources managed herein"
-  type = "map"
+  type        = "map"
+
   default = {
-    "Author" = "ReactiveOps"
+    "Author"     = "ReactiveOps"
     "Managed By" = "Terraform"
   }
 }
